87bb7535b7d517cada8ffd89a0c1c20e16cdc292
TheoB78 <theo.baustaedter@gmx.at>
2024-10-12 19:33:29 +0200

    statistic view

--- web_shop_frontend/src/services/ProductService.ts
+++ web_shop_frontend/src/services/ProductService.ts
@@ -1,44 +1,65 @@
-import Product from "@/models/Product"
+import Product from '@/models/Product'
 import axios from 'axios'
 
 export default class ProductService {
-    baseHref = 'http://localhost:8080'
+  baseHref = 'http://localhost:8080'
 
-    async getAllProducts() {
-        const products: Product[] = []
-    
-        try {
-          await axios
-            .get(`${this.baseHref}/products`)
-            .then((response) => {
-              for (const item of response.data) {
-                products.push(new Product(item))
-              }
-            })
-            .catch((error) => {
-              alert(`Error message: ${error.message}\nCode: ${error.code}`)
-            })
-        } catch (error: any) {
-          alert(`Error message: ${error.message}\nCode: ${error.code}`)
-        }
-        return products;
-      }
+  async getAllProducts() {
+    const products: Product[] = []
 
-      async getProductById(id: Number) {
-        let product
-        try {
-          await axios
-            .get(`${this.baseHref}/product`, { params: { id: id } })
-            .then((response) => {
-              product = new Product(response.data)
-            })
-            .catch((error) => {
-              console.log(error)
-              alert(`Error message: ${error.message}\nCode: ${error.code}`)
-            })
-        } catch (error: any) {
+    try {
+      await axios
+        .get(`${this.baseHref}/products`)
+        .then((response) => {
+          for (const item of response.data) {
+            products.push(new Product(item))
+          }
+        })
+        .catch((error) => {
           alert(`Error message: ${error.message}\nCode: ${error.code}`)
-        }
-        return product
-      }
+        })
+    } catch (error: any) {
+      alert(`Error message: ${error.message}\nCode: ${error.code}`)
+    }
+    return products
+  }
+
+  async getProductById(id: Number) {
+    let product
+    try {
+      await axios
+        .get(`${this.baseHref}/product`, { params: { id: id } })
+        .then((response) => {
+          product = new Product(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+          alert(`Error message: ${error.message}\nCode: ${error.code}`)
+        })
+    } catch (error: any) {
+      alert(`Error message: ${error.message}\nCode: ${error.code}`)
+    }
+    return product
+  }
+
+  async getProductStatistics(ascending: boolean = false, limit: Number = 5, offset: Number = 0) {
+    let results = {}
+
+    try {
+      await axios
+        .get(`${this.baseHref}/topProducts`, {
+          params: { ascending: ascending, limit: limit, offset: offset }
+        })
+        .then((response) => {
+          results = response
+        })
+        .catch((error) => {
+          console.log(error)
+          alert(`Error message: ${error.message}\nCode: ${error.code}`)
+        })
+    } catch (error: any) {
+      alert(`Error message: ${error.message}\nCode: ${error.code}`)
+    }
+    return results
+  }
 }
